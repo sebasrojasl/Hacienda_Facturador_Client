41c410eb9d99372505b75569381aab276e265858
moraalejandro94 <marian.soza99@gmail.com>
2021-05-10 22:19:27 -0600

    Update BillVisualizer.js

--- src/components/BillVisualizer.js
+++ src/components/BillVisualizer.js
@@ -5,9 +5,130 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import { DataGrid } from '@material-ui/data-grid';
-import FormData from "form-data"
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Box from '@material-ui/core/Box';
+import Collapse from '@material-ui/core/Collapse';
+import IconButton from '@material-ui/core/IconButton';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 import axios from 'axios';
+
+
+const useRowStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset',
+    },
+  },
+});
+
+function createData(name, Fecha, Emisor, Receptor, IVA, Total) {
+  return {
+    name,
+    Fecha,
+    Emisor,
+    Receptor,
+    IVA,
+    Total,
+    history: [
+      { date: '2020-01-05', customerId: '11091700', amount: 3 },
+      { date: '2020-01-02', customerId: 'Anonymous', amount: 1 },
+    ],
+  };
+}
+
+function Row(props) {
+  const { row } = props;
+  const [open, setOpen] = React.useState(false);
+  const classes = useRowStyles();
+
+  return (
+    <React.Fragment>
+      <TableRow className={classes.root}>
+        <TableCell style={{width:5, marginLeft:8}}>
+          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
+            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+          </IconButton>
+        </TableCell>
+        <TableCell component="th" scope="row">
+          {row.name}
+        </TableCell>
+        <TableCell align="center" style={{width:3}}>{row.Fecha}</TableCell>
+        <TableCell align="center">{row.Emisor}</TableCell>
+        <TableCell align="center">{row.Receptor}</TableCell>
+        <TableCell align="center">{row.IVA}</TableCell>
+      </TableRow>
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={open} timeout="auto" unmountOnExit>
+            <Box margin={1}>
+              <Typography variant="h6" gutterBottom component="div">
+                History
+              </Typography>
+              <Table size="small" aria-label="purchases">
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Date</TableCell>
+                    <TableCell>Customer</TableCell>
+                    <TableCell align="center">Amount</TableCell>
+                    <TableCell align="center">Total Total ($)</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {row.history.map((historyRow) => (
+                    <TableRow key={historyRow.date}>
+                      <TableCell component="th" scope="row">
+                        {historyRow.date}
+                      </TableCell>
+                      <TableCell>{historyRow.customerId}</TableCell>
+                      <TableCell align="center">{historyRow.amount}</TableCell>
+                      <TableCell align="center">
+                        {Math.round(historyRow.amount * row.Total * 100) / 100}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Box>
+          </Collapse>
+        </TableCell>
+      </TableRow>
+    </React.Fragment>
+  );
+}
+
+Row.propTypes = {
+  row: PropTypes.shape({
+    Fecha: PropTypes.number.isRequired,
+    Receptor: PropTypes.number.isRequired,
+    Emisor: PropTypes.number.isRequired,
+    history: PropTypes.arrayOf(
+      PropTypes.shape({
+        amount: PropTypes.number.isRequired,
+        customerId: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      }),
+    ).isRequired,
+    name: PropTypes.string.isRequired,
+    Total: PropTypes.number.isRequired,
+    IVA: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
+  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
+  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
+  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
+];
 
 
 const useStyles = (theme) => ({
@@ -34,16 +155,7 @@ const useStyles = (theme) => ({
       marginRight: 'auto',
     },
   },
-  paper: {
-    marginTop: theme.spacing(3),
-    marginBottom: theme.spacing(3),
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      marginTop: theme.spacing(6),
-      marginBottom: theme.spacing(6),
-      padding: theme.spacing(3),
-    },
-  },
+
   buttons: {
     display: 'flex',
     justifyContent: 'flex-end',
@@ -66,236 +178,99 @@ const useStyles = (theme) => ({
 
 class BillVisualizer extends Component {
 
-  act_paymentType= {'Contado':'01', 'Crédito':'02', 'Consignación':'03', 'Apartado':'04', 'Arrendamiento con opción de compra':'05', 'Arrendamiento en función financiera':'06', 'Cobro a favor de un tercero':'07', 'Servicios prestados al Estado a crédito':'08', 'Pago de servicios prestados al Estado':'09', 'Otros':'09'};
+    act_paymentType= {'Contado':'01', 'Crédito':'02', 'Consignación':'03', 'Apartado':'04', 'Arrendamiento con opción de compra':'05', 'Arrendamiento en función financiera':'06', 'Cobro a favor de un tercero':'07', 'Servicios prestados al Estado a crédito':'08', 'Pago de servicios prestados al Estado':'09', 'Otros':'09'};
+    
+    BackendURL = "https://haciendabackend.herokuapp.com"
   
-  BackendURL = "https://haciendabackend.herokuapp.com"
-
-  constructor(props){
-    super(props);
-    this.state = {
-      activePanel: 0,
-      products : [],
-      product: {
-        id:  '',
-        description: '',
-        unit: '',
-        quantity: '',
-        price: '',
-      },
-      inputValue:'',
-      errors: [],
-      error: false,
-      helperText: '' 
-      
-    }
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleValidationBranch(){
-    
-  } 
-
-  handleChange(event) {
-    if(event.target.value.length > 5){
-      this.setState({[event.target.name]:event.target.value});
-      //this.setState({helperText:"", error: false});
-    }else{
-      this.setState({[event.target.name]:event.target.value});
-      //this.setState({helperText:"Invalid", error: true});
-    }
-    
-  }
-
-  handleNewProduct = name => ({target:{value}}) => {
-    this.setState({
-      product: {
-        ...this.state.product,
-        [name] : value
+    constructor(props){
+      super(props);
+      this.state = {
+        activePanel: 0,
+        products : [],
+        product: {
+          id:  '',
+          description: '',
+          unit: '',
+          quantity: '',
+          Total: '',
+        },
+        inputValue:'',
+        errors: [],
+        error: false,
+        helperText: '' 
+        
       }
-    })
-  }
-
-  handleComboBoxChange(event, value) {
-    this.setState({[event]:value});
-  }
-
-  handleSubmit(event) {
-    var body = new FormData();
-    
-    /*Datos emisor*/
-      body.append("emitterName", this.state.name_emisor);
-      body.append("emitterIDType", this.state.idEmisor_combobox_Value);
-      body.append("emitterID", this.state.idnumber);
-      body.append("emitterPhone", this.state.phone_emitter);
-      body.append("emitterAddressDrescription", this.state.address_emitter);
-      body.append("emitterEmail", this.state.email_emisor);
-
-      body.append("emitterProvince", this.state.emitterProvince);
-      body.append("emitterCanton", this.state.emitterCanton);
-      body.append("emitterDistrict", this.state.emitterDistrict);
-      body.append("emitterNeighborhood", this.state.emitterNeighborhood);
-      body.append("emitterCountryCode", this.state.emitterCountryCode);
-      body.append("emitterBuisnessName", this.state.emitterBusinessName);
-    //
-
-    /* Datos receptor */
-      body.append("receiverName", this.state.name_receptor);
-      body.append("receiverIDType", this.state.idReceptor_combobox_inputValue);
-      body.append("receiverID", this.state.idnumber_receptor);
-      body.append("receiverEmail", this.state.email_receptor);
-
-      /* Faltan */ 
-      body.append("receiverProvince", this.state.receiverProvince);
-      body.append("receiverCanton", this.state.receiverCanton);
-      body.append("receiverDistrict", this.state.receiverDistrict);
-      body.append("receiverNeighborhood", this.state.receiverNeighborhood);
-      body.append("receiverCountryCode", this.state.receiverCountryCode);
-      body.append("receiverPhone", this.state.receiverPhone);
-    //
-
-    /* Datos para consecutivo */
-      body.append("terminal", this.state.id_caja);
-      body.append("subsidiary", this.state.id_sucursal);
-    //
-
-    /* Datos para factura*/
-      body.append("sellCondition", );
-      body.append("creditTerm", this.state.id_credit_time);
-      body.append("payMethod", this.state.payMethod_combobox_inputValue);
-      body.append("currencyCode", this.state.currencyType_combobox_inputValue);
-      body.append("exchangeRate", this.state.currency);
-      //body.append("", this.state.);
-      //body.append("", this.state.);
-      //body.append("", this.state.);
-
-    axios({
-      method: "post",
-      url: this.BackendURL,
-      data: body,
-      headers: { "Content-Type": "multipart/form-data" },
-    })
-      .then(function (response) {
-        //handle success
-        console.log(response);
-      })
-      .catch(function (response) {
-        //handle error
-        console.log(response);
-      });
-    event.preventDefault();
-  }
-
-  handleNewLine = () => { 
-    const {product, products, inputValue} = this.state;
-    //this.props.onCreate(product);
-    product.unit = inputValue;
-    this.setState({
-      products:[
-        ...products,
-        product
-      ]
-    }) 
-
-    this.setState({
-      product: {
-        id:  '',
-        description: '',
-        unit: '',
-        quantity: '',
-        price: '',
-      },
-      inputValue: ''
-    })
-  }
-
-  swapFormActive = (param) => () => {
-    this.setState({
-      activePanel: param
-    });
-  }
-
-  handleNext = () => {
-    const { activePanel } = this.state;
-    this.setState({
-      activePanel: activePanel + 1
-    });
-  };
-
-  handleBack = () => {
-    const { activePanel } = this.state;
-    this.setState({
-      activePanel: activePanel - 1
-    });
-  };
-
-
-  render() {
-  const classes = this.props.classes;
-  const {product: {id, description, quantity, price}, inputValue} = this.state;
-
-  //const  rowsProduct = ['id', 'description','unit', 'quantity' , 'price'];
-
-
-  const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
-    {
-      field: 'age',
-      headerName: 'Age',
-      type: 'number',
-      width: 90,
-    },
-    {
-      field: 'fullName',
-      headerName: 'Full name',
-      description: 'This column has a value getter and is not sortable.',
-      sortable: false,
-      width: 160,
-      valueGetter: (params) =>
-        `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-    },
-  ];
+    }
   
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-  ];
+    handleValidationBranch(){
+      
+    } 
+  
+    handleSubmit(event) {
+      var body = new FormData();
+  
+      axios({
+        method: "post",
+        url: this.BackendURL,
+        data: body,
+        headers: { "Content-Type": "multipart/form-data" },
+      })
+        .then(function (response) {
+          //handle success
+          console.log(response);
+        })
+        .catch(function (response) {
+          //handle error
+          console.log(response);
+        });
+      event.preventDefault();
+    }
+
+  
+  
+    render() {
+    const classes = this.props.classes;
 
 
   return (
     <React.Fragment>
-      <CssBaseline />
-      <AppBar position="absolute" className={classes.appBar}>
-        <Toolbar>
-          <Typography variant="h6" color="inherit" noWrap>
-            Visualizador de facturas
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main className={classes.layout}>
-        <Paper className={classes.paper}>
-          <Typography component="h1" variant="h4" align="center">
-          Facturas previas
-          </Typography>
-            <div style={{ height: 600, width: '100%' }}>
-                <DataGrid rows={rows} columns={columns} pageSize={20} checkboxSelection />
-            </div>         
+    <CssBaseline />
+    <AppBar position="absolute" className={classes.appBar}>
+      <Toolbar>
+        <Typography variant="h6" color="inherit" noWrap>
+          Visualizador de facturas
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <main className={classes.layout}>
+      <Paper className={classes.paper}>
+        <React.Fragment>
+        <TableContainer component={Paper}>
+            <Table aria-label="collapsible table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell />
+                        <TableCell width='15'>Fecha</TableCell>
+                        <TableCell align="center">Emisor</TableCell>
+                        <TableCell align="center">Receptor</TableCell>
+                        <TableCell align="center">IVA</TableCell>
+                        <TableCell align="center">Total</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {rows.map((row) => (
+                        <Row key={row.name} row={row} />
+                    ))}
+                </TableBody>
+            </Table>
+            </TableContainer>
+        </React.Fragment>
         </Paper>
       </main>
     </React.Fragment>
   );
   }
-}
+};
 
 
 
